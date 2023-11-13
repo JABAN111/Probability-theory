d53606c97b3c31015c5eca30ddff3b31ae16cb1b
Jaba <368601@edu.itmo.ru>
2023-11-13 21:36:48 +0300

    added function extremes,scope,variation series

--- rowManipulator.py
+++ rowManipulator.py
@@ -1,4 +1,3 @@
-
 '''
 Класс для обработки и взаимодействия с выборкой
 '''
@@ -7,5 +6,27 @@
 class rowManipulator:
     def __init__(self, row):
         self.row = row
+
+    '''
+    Возвращает вариационный ряд
+    '''
+
     def variation_series(self):
         return sorted(self.row)
+
+    '''
+    Возвращает экстремальные значения
+    '''
+
+    def extremes(self):
+        min_value = str(self.variation_series()[0])
+        max_value = str(self.variation_series()[-1])
+        return "Минимальное значение: " + min_value + ", максимальное значение: " + max_value
+
+    '''
+    Возвращает размах выборки
+    '''
+    def scope(self):
+        min_value = self.variation_series()[0]
+        max_value = self.variation_series()[-1]
+        return max_value - min_value
