8e26414a27d50571c38ff0e0ae24d63c9bae7d89
Jaba <368601@edu.itmo.ru>
2023-11-13 21:16:04 +0300

    added row for eight variant

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from math import log
+from prettytable import PrettyTable
+import matplotlib.pyplot as plt
+from rowManipulator import rowManipulator
+#выборка восьмого варианта
+rowOfEighthVariant =[1.07, 1.59,
+                     -1.49, -0.10,
+                    0.11,1.18,
+                     0.35,-0.73,
+                     1.07,0.31,
+                     -0.26,-1.20,
+                     -0.35,0.73,
+                     1.01,-0.12,
+                     0.28,-1.32,
+                     -1.10,-0.26]
+rowController = rowManipulator(rowOfEighthVariant)
+print(rowController.manipulate_row())
