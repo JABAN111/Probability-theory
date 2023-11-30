6db22a271ca4adf029faed3c6d40f896df706d02
Jaba <368601@edu.itmo.ru>
2023-11-30 23:02:04 +0300

    пофикшена проблема с некоректным вычислением эмпирической функции

--- rowManipulator.py
+++ rowManipulator.py
@@ -5,6 +5,7 @@
 import matplotlib.pyplot as plt
 from prettytable import PrettyTable
 from math import log
+import numpy as np
 
 
 class rowManipulator:
@@ -12,33 +13,28 @@ class rowManipulator:
         self.row = row
         self.grouped_data = None
 
-
-    '''
-    Возвращает вариационный ряд
-    '''
-
     def variation_series(self):
+        """
+        Возвращает вариационный ряд
+        """
+
         return sorted(self.row)
 
-    '''
-    Возвращает экстремальные значения
-    '''
-
     def extremes(self):
+        """
+       Возвращает экстремальные значения
+       """
         min_value = str(self.variation_series()[0])
         max_value = str(self.variation_series()[-1])
         return "Минимальное значение: " + min_value + ", Максимальное значение: " + max_value
 
-    '''
-    :return размах выборки
-    '''
-
     def scope(self):
+        """
+        :return размах выборки
+        """
         min_value = self.variation_series()[0]
         max_value = self.variation_series()[-1]
         return max_value - min_value
-
-
 
     def Statistical_series(self):
         """
@@ -48,8 +44,8 @@ class rowManipulator:
         sorted_row = self.variation_series()
 
         for x in sorted_row:
-            #пометка для себя: проверяем последний ключ, тк если элемент повторяется, то он будет подряд и не нужно
-            #весь массив
+            # пометка для себя: проверяем последний ключ, тк если элемент повторяется, то он будет подряд и не нужно
+            # весь массив
             if count_set and x == list(count_set.keys())[-1]:
                 count_set[x] += 1
             else:
@@ -61,15 +57,11 @@ class rowManipulator:
         Вычисляет относительную частоту для статического ряда
         :return: возвращает массив со значениями p(i) для статического ряда
         """
-        p = []
         n = len(self.variation_series())
         count_set = self.Statistical_series()
-        # p = [i // n for i in list(count_set.values())]
-        for i in list(count_set.values()):
-            p.append(i/n)
-            # print("текущее значение value: " + i + "результат его вычесления: " + str(i/n))
-        # print(p)
+        p = [float(format(i / n, '.2f')) for i in list(count_set.values())]
         return p
+
     def printer_Statistical_series(self):
         """
         Выводит в стандартный поток вывода красивую табличку
@@ -78,37 +70,34 @@ class rowManipulator:
         table = PrettyTable()
         table.field_names = ["x(i)", *count_set.keys()]
         table.add_row(["n(i)", *count_set.values()])
-
         table.add_row(["p(i)", *self.relative_frequency()])
         return table
 
-    '''
-    :return среднее значение выборки
-    '''
-
-    def avgRow(self):
+    def avg_row(self):
+        """
+        :return среднее значение выборки
+        """
         return sum(self.row) / len(self.row)
 
-    '''
-    :return оценку мат ожидания выборки
-    '''
-
     def assessment_mathematical_expectation(self):
+        """
+        :return оценку мат ожидания выборки
+        """
         n = len(self.row)
         count_set = self.Statistical_series()
         total = 0
         for x in count_set:
-            total += x*count_set[x]
-        return total/n
-
-    '''
-    :return дисперсию выборки
-    '''
+            total += x * count_set[x]
+            total = round(total, 2)
+        return total / n
 
     def dispersion(self):
+        """
+        :return дисперсию выборки
+        """
         dispersion = 0
         for x in self.row:
-            dispersion += (x - self.avgRow()) * (x - self.avgRow())
+            dispersion += (x - self.avg_row()) * (x - self.avg_row())
         return dispersion
 
     '''
@@ -118,32 +107,33 @@ class rowManipulator:
     def standard_deviation(self):
         return self.dispersion() ** 0.5
 
-
-
     def empirical_distribution(self):
         """
        :return эмпирическую функцию распределения и график к ней
        """
         count_set = self.Statistical_series()
+        print(len(self.variation_series()))
         plt.subplot(5, 1, 1)
         plt.title("График эмпирической функции распределения")
         n = len(count_set)
         keys = list(count_set.keys())
         y = 0
-        print(f'\t\t/ {round(y, 2)}, при x <= {keys[0]}')
+        frequencyArray = self.relative_frequency()
+        print(f'\t\t/ {y}, при x <= {keys[0]}')
         result = ""
-        for i in range(n-1):
+        for i in range(n - 1):
             if i < n:
-                y += count_set[keys[i]] / n
+                y += frequencyArray[i]
+            #if+else для красивого вывода F(x)
             if i == n / 2:
                 left = "F*(x) = "
             else:
                 left = "\t\t"
-            result += f'{left}| {round(y, 2)}, при {keys[i]} < x <= {keys[i + 1]}\n'
+            result += f'{left}| {round(y,2)}, при {keys[i]} < x <= {keys[i + 1]}\n'
             plt.plot([keys[i], keys[i + 1]], [y, y], c='black')
-        result += f'\t\t\\ {round(y, 2)}, при {keys[-1]} < x'
-        return result;
-
+        y += frequencyArray[-1]
+        result += f'\t\t\\ {round(y,2)}, при {keys[-1]} < x'
+        return result
 
     '''
     Интервальное статистическое распределение
@@ -182,15 +172,15 @@ class rowManipulator:
     '''
     Полигон частот
     '''
+
     def frequency_polygon(self):
         plt.subplot(5, 1, 3)
         plt.title("Полигон приведенных частот")
-        if(self.grouped_data != None):
+        if (self.grouped_data != None):
             plt.plot(list(self.grouped_data.keys()), list(self.grouped_data.values()), c='red')
         else:
             self.interval_statistical_distribution()
             self.frequency_polygon()
-
 
     def frequency_histogram(self):
         '''
@@ -211,13 +201,13 @@ class rowManipulator:
         y_values = list(self.grouped_data.values())
         plt.bar(x_values, y_values, width=h)
 
-        xticks = [round(x + h,2) for x in self.grouped_data.keys()]
-        xticks.insert(0,round(list(self.grouped_data.keys())[0],2))
+        xticks = [round(x + h, 2) for x in self.grouped_data.keys()]
+        xticks.insert(0, round(list(self.grouped_data.keys())[0], 2))
         plt.xticks(xticks, xticks)
-
 
     '''
     Отображает все графики, сделанные plt
     '''
+
     def show_images(self):
         plt.show()
